4888906a7d4dd799c00aa35a5b0d20e36890af9f
Pathfinder <bradenjbuchanan@gmail.com>
2023-04-01 20:43:33 -0400

    adding password protection for projects

--- password.js
+++ password.js
@@ -1,29 +1,32 @@
-document.querySelector('#project-1').addEventListener('click', checkPassword);
+'use strict';
 
-function checkPassword() {
-    const enteredPassword = prompt('Enter password:');
-    const hashedEnteredPassword = CryptoJS.SHA256(enteredPassword).toString();
-    const hashedCorrectPassword = 'ab7d0a36ecbe10c286f92a49bffb57ed6bf5bc6c3a3e15967f3ea8d028316969';
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#project-1').addEventListener('click', checkPassword1);
+  document.querySelector('#project-2').addEventListener('click', checkPassword2);
+});
+
+function checkPassword1() {
+  const enteredPassword = prompt('Enter password:');
+  const hashedEnteredPassword = CryptoJS.SHA256(enteredPassword).toString();
+  const hashedCorrectPassword = '0cedbd78e59e9963e3e43ad5455eae46b222a4b6a6e9189856d2b691651a08f0';
     
-    if (hashedEnteredPassword === hashedCorrectPassword) {
-        window.location.href = 'airbnb-jtbd.html';
-    } else {
-        alert('Incorrect password, please try again.');
-        event.preventDefault(); // prevent the default link behavior
-    }
+  if (hashedEnteredPassword === hashedCorrectPassword) {
+    window.location.href = 'airbnb-jtbd.html';
+  } else {
+    alert('Incorrect password, please try again.');
+    event.preventDefault(); // prevent the default link behavior
+  }
 }
 
-document.querySelector('#project-2').addEventListener('click', checkPassword);
-
-function checkPassword() {
-    const enteredPassword = prompt('Enter password:');
-    const hashedEnteredPassword = CryptoJS.SHA256(enteredPassword).toString();
-    const hashedCorrectPassword = '46aa338f8d9f28dd20de3540259fe3fb45d83cfb4bf222e4eb94975e13f4990c';
+function checkPassword2() {
+  const enteredPassword = prompt('Enter password:');
+  const hashedEnteredPassword = CryptoJS.SHA256(enteredPassword).toString();
+  const hashedCorrectPassword = '46aa338f8d9f28dd20de3540259fe3fb45d83cfb4bf222e4eb94975e13f4990c';
     
-    if (hashedEnteredPassword === hashedCorrectPassword) {
-        window.location.href = 'airbnb-jtbd.html';
-    } else {
-        alert('Incorrect password, please try again.');
-        event.preventDefault(); // prevent the default link behavior
-    }
+  if (hashedEnteredPassword === hashedCorrectPassword) {
+    window.location.href = 'blank.html';
+  } else {
+    alert('Incorrect password, please try again.');
+    event.preventDefault(); // prevent the default link behavior
+  }
 }
